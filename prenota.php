0cd4ea9ec5a900242f4fbf9bef8b11878e5530be
Huber <hubersimone01@gmail.com>
2021-03-06 12:31:08 +0100

    Add SQL insertion example

--- prenota.php
+++ prenota.php
@@ -6,4 +6,31 @@
 ini_set('display_errors', 1);
 ini_set('log_errors', 0);
 
+$host = 'localhost';
+$db = 'tamponi';
+$user = 'root';
+$pass = '';
 
+//stringa di connessione
+$dsn = 'mysql:host' . $host . 'dbname=' . $db;
+
+
+$pdo = new PDO($dsn, $user, $pass);
+
+//Variabili al momento costanto poi verranno prese tramite POST
+$codice_fiscale = 'HUSBRT30T999DR54';
+$giorno='2021-03-06';
+
+//Query di iserimento preparata
+$sql = "INSERT INTO prenotazioni VALUES(null, :cod_fiscale, :giorno)"; // :cod_fiscale :giorno sono dei segnaposti che andranno riempiti
+
+//Inaviamo la query al database che la tiene in pancia
+$stmt = $pdo ->prepare($sql);
+
+//Inviamo i dati concreti che verranno messi al posto dei segnaposto
+$stmt-> execute(
+    [
+        'cod_fiscale'=> $codice_fiscale,
+        'giorno' => $giorno
+    ]
+);
