46f153e44ddcf3dc305b75d7a81bb104a78659ba
Huber <hubersimone01@gmail.com>
2021-03-09 12:44:44 +0100

    Correction of an error in  prenota.php file

--- prenota.php
+++ prenota.php
@@ -1,36 +1,41 @@
 <?php
 
-
 //Dice a livello dello script che gli errori verranno mostrati
-// e che non verranno loggati
-ini_set('display_errors', 1);
-ini_set('log_errors', 0);
+//e che non verranno loggati
+ini_set("display_errors", 1);
+ini_set("log_errors", 0);
 
 $host = 'localhost';
 $db = 'tamponi';
 $user = 'root';
 $pass = '';
 
-//stringa di connessione
-$dsn = 'mysql:host' . $host . 'dbname=' . $db;
-
+//Stringa di connessione
+$dsn = 'mysql:host=' . $host . ';dbname=' . $db;
 
 $pdo = new PDO($dsn, $user, $pass);
+//Variabili valorizzate tramite POST
 
-//Variabili al momento costanto poi verranno prese tramite POST
-$codice_fiscale = $_POST['codice'];
-$giorno= $_POST['giorno'];
+$codice_fiscale = $_POST ['codice'] ;
+$giorno = $_POST['giorno'];
 
-//Query di iserimento preparata
-$sql = "INSERT INTO prenotazioni VALUES(null, :cod_fiscale, :giorno)"; // :cod_fiscale :giorno sono dei segnaposti che andranno riempiti
+//Query di inserimento preparate
 
-//Inaviamo la query al database che la tiene in pancia
-$stmt = $pdo ->prepare($sql);
+$sql = "INSERT INTO prenotazioni VALUES(null, :codice_fiscale, :giorno)";
+
+//Inviamo la query al db che la tiene in pancia
+$stmt = $pdo-> prepare($sql);
 
 //Inviamo i dati concreti che verranno messi al posto dei segnaposto
-$stmt-> execute(
+$stmt -> execute(
     [
-        'cod_fiscale'=> $codice_fiscale,
+        'codice_fiscale' => $codice_fiscale,
         'giorno' => $giorno
     ]
 );
+
+echo "Inserimento riuscito";
+
+//Ridirige il browser verso la pagina indicata nella location
+header('Location : lista_prenotazioni.php');
+exit(0);
